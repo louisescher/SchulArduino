a58641946fd88cd5e8851113b4e99243238d8fd4
SpiritLetsPlays <spiritelectro.kontakt@gmail.com>
2021-09-21 17:40:30 +0200

    Update HA_20.09.21_4LED_Blinkprogramm.ino

--- HA_20.09.21_4LED_Blinkprogramm.ino
+++ HA_20.09.21_4LED_Blinkprogramm.ino
@@ -5,33 +5,32 @@
  * Louis Escher
  */
 
- int LED1 = 13;  
- int LED2 = 12; 
- int LED3 = 11; 
- int LED4 = 10; 
- 
+ int LED1 = 13;
+ int LED2 = 12;
+ int LED3 = 11;
+ int LED4 = 10;
+
  int delayMS = 1000;
 
-
  
-void setup() {  
-  pinMode(LED1, OUTPUT); 
-  pinMode(LED2, OUTPUT); 
-  pinMode(LED3, OUTPUT); 
-  pinMode(LED4, OUTPUT); 
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 }
 
-void loop() { 
-  digitalWrite(LED1, HIGH); 
-  delay(delayMS); 
+void loop() {
+  digitalWrite(LED1, HIGH);
+  delay(delayMS);
   digitalWrite(LED1, LOW);
-  digitalWrite(LED2, HIGH); 
-  delay(delayMS); 
+  digitalWrite(LED2, HIGH);
+  delay(delayMS);
   digitalWrite(LED2, LOW);
-  digitalWrite(LED3, HIGH); 
-  delay(delayMS); 
+  digitalWrite(LED3, HIGH);
+  delay(delayMS);
   digitalWrite(LED3, LOW);
-  digitalWrite(LED4, HIGH); 
-  delay(delayMS); 
+  digitalWrite(LED4, HIGH);
+  delay(delayMS);
   digitalWrite(LED4, LOW);
 }
