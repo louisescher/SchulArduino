e9970eb0a6ebd60e1f55c104e2b160522ceaae58
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2021-09-27 10:44:32 +0200

    Update S8_A1_Rechner.ino

--- S8_A1_Rechner/S8_A1_Rechner.ino
+++ S8_A1_Rechner/S8_A1_Rechner.ino
@@ -7,10 +7,25 @@
 int a = 1000; // Nummer 1
 int b = 2000; // Nummer 2
 int X = a + b; // Rechnung
+int delayMS = 1000;
 
 void setup() {
   Serial.begin(9600); // Kommunikationsrate mit Arduino
+  Serial.print("Zahl 1 (a): "); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(a); // Print vom Ergebnis der Rechnung (Variable X)
+  delay(delayMS);
+  Serial.print("Zahl 2 (b): "); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(b); // Print vom Ergebnis der Rechnung (Variable X)
+  delay(delayMS);
+  Serial.print("Rechne "); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(a); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(" und "); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(b); // Print vom Ergebnis der Rechnung (Variable X)
+  Serial.println(" zusammen."); // Print vom Ergebnis der Rechnung (Variable X)
+  delay(delayMS);
+  Serial.print("Ergebnis: "); // Print vom Ergebnis der Rechnung (Variable X)
   Serial.println(X); // Print vom Ergebnis der Rechnung (Variable X)
+  delay(delayMS);
 }
 void loop() {
   
