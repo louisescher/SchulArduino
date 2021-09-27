12fd01104bfe7052024d280b54dc05665b454d04
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2021-09-27 10:06:26 +0200

    Add files via upload

---
+++ S7_BSP/S7_BSP.ino
@@ -0,0 +1,17 @@
+/*
+ * Hallo Welt!
+ * Dieser Text soll auf dem Bildschirm des PCs angezeigt werden.
+ * Flach mein Name.
+ * 27.09.2021
+ */
+
+ void setup() {   
+  Serial.begin(9600); // Beginne nach 9.6 Sekunden
+
+  Serial.println("Hallo Welt, könnt ihr mich hören?"); // String wird in Konsole ausgegeben
+ }
+
+void loop()
+{
+  //
+}
