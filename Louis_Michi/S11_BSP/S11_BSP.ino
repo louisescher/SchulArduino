10576c914867eaa95fa49b04944a10db0c08301b
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2021-10-04 10:23:56 +0200

    Add files via upload

--- Louis_Michi/S11_BSP/S11_BSP.ino
+++ Louis_Michi/S11_BSP/S11_BSP.ino
@@ -5,11 +5,27 @@
  */
 
 int LED = 11;
+int Dimm = 1; 
+int delayMS = 100; 
 
 void setup() {
   Serial.begin(9600);
+  for(Dimm; Dimm<256; Dimm++) {
+    analogWrite(LED, Dimm);
+    Serial.print("LED auf Helligkeitsstufe ");
+    Serial.print(Dimm);
+    Serial.println(" / 255");
+    delay(delayMS);
+  }
+  for(Dimm; Dimm>0; Dimm--) {
+    analogWrite(LED, Dimm);
+    Serial.print("LED auf Helligkeitsstufe ");
+    Serial.print(Dimm);
+    Serial.println(" / 255");
+    delay(delayMS);
+  }
+  Serial.println("Fertig");
 }
 
 void loop() {
-  analogWrite(LED, 1);
 }
