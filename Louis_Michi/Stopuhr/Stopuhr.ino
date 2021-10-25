55ea1730be64ec1cc5827ec20108d7214fcdd075
Spirit <66965600+SpiritLetsPlays@users.noreply.github.com>
2021-10-25 10:46:05 +0200

    Update Stopuhr.ino

--- Louis_Michi/Stopuhr/Stopuhr.ino
+++ Louis_Michi/Stopuhr/Stopuhr.ino
@@ -10,6 +10,8 @@ const int rs = 11, en = 12, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int Timer;
+int TimerMins = 0;
+int TimerHours = 0;
 
 int StartTimerButton = 8;
 int PauseTimerButton = 9;
@@ -39,6 +41,8 @@ void loop() {
           isTimerRunning = true;
         } else {                                                                    // If the Timer is not Running but IS paused
           Timer = 0;                                                                // Set Timer to 0
+          TimerMins = 0;
+          TimerHours = 0;
           isTimerRunning = false;
           isTimerPaused = false;
         }
@@ -72,10 +76,25 @@ void loop() {
   if(isTimerPaused == true) {                                                       // If the Timer is Paused, do nothing
     //Nothing here
   }
+
+  if(Timer >= 60) {                                                                 // If the Seconds counter is 60 or above, add a Minute and clear the seconds
+    TimerMins++;
+    Timer = 0;
+  }
+  
+  if(TimerMins >= 60) {                                                             // If the Minutes counter is 60 or above, add a hour and clear the minutes
+    TimerHours++;
+    TimerMins = 0;
+  }
   
    lcd.print(TimerStateString);                                                     // Print in the First Line 
    lcd.setCursor(0, 1);                                                             // Set Cursor in next Line
-   lcd.print(Timer);                                                                // Print current Time
-   delay(1000);                                                                      // Wait a second (Not exactly a second here to allow for delays happening in the code and because of the slow computing speed on the arduino)
+   lcd.print(TimerHours);                                                           // Print Hours
+   lcd.print("h, ");                                                                // String
+   lcd.print(TimerMins);                                                            // Print Minutes
+   lcd.print("m, ");                                                                // String
+   lcd.print(Timer);                                                                // Print Seconds
+   lcd.print("s");                                                                  // String
+   delay(980);                                                                      // Wait a second (Not exactly a second here to allow for delays happening in the code and because of the slow computing speed on the arduino)
    lcd.clear();                                                                     // Clear Display to allow for a new loop
 }
